76799d68b6ab2cc84705b6e9c26906361d8da799
Alex Marineau <alexmarineau@yahoo.com>
2015-09-08 21:27:24 -0700

    draws more squares

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,45 @@
+var squareSize = 10;
+
 $(document).ready(function() {
+	
+	setup();
+	hoverSetup();
+});
+
+function setup()
+{
+	var s; 
+	var check = true;
+	var mainSize = parseInt($('#mainSquare').css('height'));
+
+	while(check)
+	{
+		s = mainSize/squareSize;
+
+		if(s % 1 == 0)
+		{
+			check = false;
+		}
+		else
+		{
+			mainSize -= 1;
+		}
+		console.log(mainSize);
+		console.log("" + s + " & " + s + "");
+	}
+
+	$("#mainSquare").css({
+		'height': mainSize + 'px',
+		'width': mainSize + 'px'
+	});
+
+	//console.log("" + s + " & " + s + "");
+
 	var content = "<table>";
 
-	for (var i = 0; i < 16; i++) {
+	for (var i = 0; i < squareSize; i++) {
 		content += "<tr>";
-		for(var j = 0; j < 16; j++){
+		for(var j = 0; j < squareSize; j++){
 			content += "<td><div class=\"squares\"></div></td>";
 		}
 		content += "</tr>";
@@ -11,16 +47,28 @@ $(document).ready(function() {
 
 	content += "</table>"
 
+	
 	$("#mainSquare").append(content);
 
+	$(".squares").css({
+		"height": s + "px",
+		"width": s + "px"
+	});
+	$(".hoveredSquares").css({
+		"height": s + "px",
+		"width": s + "px"
+	});
+}
+
+function hoverSetup()
+{
 	$(".squares").hover(function() {
 		$(this).addClass('hoveredSquares');
 	}, function() {	
 		/* Stuff to do when the mouse leaves the element */	
-		$(this).removeClass('hoveredSquares')
+		//$(this).removeClass('hoveredSquares')
 	});
-
-});
+}
 
 
 
